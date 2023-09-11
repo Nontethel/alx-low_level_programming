dcf995316541e26bda3fb659cfb8ba72af8c0772
nontethel <ntettyn47@gmail.com>
2023-09-11 14:01:10 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -print the line of code
+ *
+ * Return: 0
+ */
+main(void)
+{
+	printf("Size of a char: %lu byte\n", sizeof(char));
+	printf("Size of an int: %lu bytes\n", sizeof(int));
+	printf("Size of a long int: %lu bytes\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes\n", sizeof(long long int));
+	printf("Size of a float: %lu bytes\n", sizeof(float));
+	printf("Size of a double: %lu bytes\n", sizeof(double));
+	printf("Size of a long double: %lu bytes\n", sizeof(long double));
+	return (0);
+}
