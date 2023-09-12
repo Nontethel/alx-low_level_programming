2320a9ad0ac45e839d3678e8271018ed4f8317dd
nontethel <ntettyn47@gmail.com>
2023-09-12 06:59:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - Entry point 
+ *main -Entry point
  *
- * return: always 1
+ *return: always 1 (success)
  */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar\n, 2015-10-19", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
