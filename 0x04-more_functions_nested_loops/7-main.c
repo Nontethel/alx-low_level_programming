066bd076b05fea1a5dc8594852892766147b99ac
nontethel <ntettyn47@gmail.com>
2023-09-14 16:33:35 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: always 0
+ */
+imt main(void)
+{
+	print_diagonal(0);
+	print_diagonal(4);
+	print_diagonal(10);
+	print_diagonal(-4);
+	return (0);
+}
